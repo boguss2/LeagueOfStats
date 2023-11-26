4b80acb313e6172b508b9b8056047a24811fff45
s25867 <s25867@pjwstk.edu.pl>
2023-11-26 15:43:21 +0100

    Lista wszystkich przedmiotów

--- demo/src/main/java/com/example/demo/controllers/ItemsController.java
+++ demo/src/main/java/com/example/demo/controllers/ItemsController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.champions.all.AllChampionsData;
+import com.example.demo.model.champions.single.Champion;
 import com.example.demo.model.items.AllItemsData;
 import com.example.demo.model.versions.AllVersionsData;
 import com.example.demo.service.ApiService;
@@ -9,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static org.springframework.util.StringUtils.capitalize;
+
 @Controller
 public class ItemsController {
     @Autowired
@@ -16,23 +20,45 @@ public class ItemsController {
     private final AllVersionsData versionsData;
 
     @Autowired
-    public ItemsController(ApiService apiService){
-        this.versionsData=apiService.fetchVersionData();
+    public ItemsController(ApiService apiService) {
+        this.versionsData = apiService.fetchVersionData();
     }
 
     @GetMapping("/items")
-    public String itemsMain(Model model){
+    public String itemsMain(Model model) {
         String itemsVersion = versionsData.getN().getItem();
 
-        AllItemsData itemsData=apiService.fetchItemsData("http://ddragon.leagueoflegends.com/cdn/"+itemsVersion+"/data/en_US/items.json");
-        model.addAttribute("itemsData", itemsData);
+        AllItemsData itemsData = apiService.fetchItemsData("http://ddragon.leagueoflegends.com/cdn/" + itemsVersion + "/data/en_US/item.json");
 
+        model.addAttribute("itemsData", itemsData.getData());
         return "mainItems";
     }
 
     @GetMapping("/items/{item}")
-    public String itemsInfo(@PathVariable(value="item") String name, Model model){
-        return "infoItem";
-    }
+    public String itemsInfo(@PathVariable(value = "item") String name, Model model) {
 
+        //POBRANIE DANYCH Z API(wersja jest pobierana z NA)
+        String itemsVersion = versionsData.getN().getItem();
+
+        AllChampionsData championsData = apiService.fetchChampionsData("http://ddragon.leagueoflegends.com/cdn/" + itemsVersion + "/data/en_US/item.json");
+
+        if (championsData.findChampion(name) != null) {
+            String formatedName = capitalize(name);
+//            Item item = apiService.fetchChampionData("https://ddragon.leagueoflegends.com/cdn/" + itemsVersion + "/data/en_US/item/" + formatedName + ".json");
+
+//            //dodac do modelu wszystko do porownania
+//            model.addAttribute("championData", champion.getChampionData(formatedName));
+//            model.addAttribute("championImage", champion.getChampionData(formatedName).getImage());
+//            model.addAttribute("championSkins", champion.getChampionData(formatedName).getSkins());
+//            model.addAttribute("championInfo", champion.getChampionData(formatedName).getInfo());
+//            model.addAttribute("championStats", champion.getChampionData(formatedName).getStats());
+//            model.addAttribute("championSpells", champion.getChampionData(formatedName).getSpells());
+//            model.addAttribute("championPassive", champion.getChampionData(formatedName).getPassive());
+
+            return "infoItem";
+        } else {
+            model.addAttribute("message", "Item not found");
+            return "mainItems";
+        }
+   }
 }
